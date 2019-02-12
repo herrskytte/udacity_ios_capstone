60edd983208295eddaec224301d97622fdef50d1
Frederik Skytte <fsk@computas.com>
2019-02-12 09:10:13 +0100

    Initial Commit

---
+++ Capstone/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Capstone
+//
+//  Created by Frederik Skytte on 12/02/2019.
+//  Copyright © 2019 Frederik Skytte. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
